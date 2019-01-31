8cce93674b71bcc25db6adcb8ab89c3bb74c9707
Andy West <andyjwest@gmail.com>
2019-01-31 10:16:20 -0500

    Switching over to Gatsby as it is a static stie

--- src/ResponseMapper.js
+++
@@ -1,5 +0,0 @@
-import {owner, repo, githubFileUrl, branch, publicDir} from './properties';
-
-export function buildGithubUrl(path) {
-    return `${githubFileUrl}/${owner}/${repo}/${branch}/${publicDir}/${path}`;
-}
