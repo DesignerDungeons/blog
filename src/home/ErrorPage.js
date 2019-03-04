48239636210a2ba2964f007dfd54175763afa1ea
Andy West <andyjwest@gmail.com>
2019-03-04 10:37:34 -0500

    Updates

--- src/home/ErrorPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ErrorPage = () => (
-    <div className='shadowed-container post'>
-        <h1 align="center">That page does not exist!</h1>
-    </div>
-);
-
-export default ErrorPage;
