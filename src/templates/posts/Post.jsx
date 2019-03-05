aa5b3f21b9a012a9e8896b3da3739c1e4e6c5620
Andy West <awest@cardinalsolutions.com>
2019-03-05 15:47:20 -0500

    pulling in the markdown when files are generated

--- src/templates/posts/Post.jsx
+++ src/templates/posts/Post.jsx
@@ -1,41 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactMarkdown from "react-markdown";
 import Layout from '../../components/Layout';
 import './post.css';
-import {withPrefix} from 'gatsby'
 
-class Post extends Component {
-    constructor(props){
-        super(props);
-        this.state ={
-            markdown: []
-        }
-    }
-
-    componentWillMount(){
-        let post = this.props.pageContext.post;
-        let path = withPrefix(`/posts/${post.category}/${post.url}`);
-        fetch(path)
-            .then(response => {
-                if (!response.ok) {
-                    throw Error(response.statusText);
-                }
-                return response.text();
-            })
-            .then(md => this.setState({markdown: md}))
-            .catch(error => console.warn(error));
-    }
-
-    render() {
-        return (
-            <Layout>
-                <div className="shadowed-container post">
-                    <ReactMarkdown escapeHtml={false} source={this.state.markdown}/>
-                </div>
-            </Layout>
-        )
-    }
-}
-
-export default Post
-
+export default ({pageContext: {post}}) =>
+    (<Layout>
+        <div className="shadowed-container post">
+            <ReactMarkdown escapeHtml={false} source={post.markdown}/>
+        </div>
+    </Layout>);
